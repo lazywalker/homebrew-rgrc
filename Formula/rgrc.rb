dc7b7885aaf288541ade5d4c370b370a66476e8a
lazywalker <lazywalkerz@gmail.com>
2025-11-29 19:43:37 +0800

    Fix download URLs to use .tar.gz format for rgrc version 0.4.3

--- Formula/rgrc.rb
+++ Formula/rgrc.rb
@@ -5,12 +5,12 @@ class Rgrc < Formula
   license "MIT"
 
   on_arm do
-    url "https://github.com/lazywalker/rgrc/releases/download/v#{version}/rgrc-aarch64-apple-darwin.zip"
+    url "https://github.com/lazywalker/rgrc/releases/download/v#{version}/rgrc-aarch64-apple-darwin.tar.gz"
     sha256 "ec4797adae1657dce4e96683275503971035aeeedd9c258db785027a54264702"
   end
 
   on_intel do
-    url "https://github.com/lazywalker/rgrc/releases/download/v#{version}/rgrc-x86_64-apple-darwin.zip"
+    url "https://github.com/lazywalker/rgrc/releases/download/v#{version}/rgrc-x86_64-apple-darwin.tar.gz"
     sha256 "5b260feb880eb2dd45e62df8cb620f78e2be1a207fb2138d3de144a872ab4950"
   end
 
